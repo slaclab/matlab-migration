45d0a14f64876c2cc94a355392e0df104b726174
george-mcintyre <george@level-n.com>
2021-12-09 15:08:10 +0100

    Separate pvaGet into matlab function for variable arguments

--- src/pvaGet.m
+++ src/pvaGet.m
@@ -1,7 +1,7 @@
 function out = pvaGet(channel, varargin)
     aidainit
     if ( length(varargin) > 0 )
-        out = edu.stanford.slac.aida.client.AidaPvaClientUtils.pvaGet(channel, varargin(1));
+        out = edu.stanford.slac.aida.client.AidaPvaClientUtils.pvaGet(channel, varargin{1});
     else
         out = edu.stanford.slac.aida.client.AidaPvaClientUtils.pvaGet(channel);
     end
