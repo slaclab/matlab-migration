3da1e2ae84f6a77330eeafb5442c4c6542fab6f0
george-mcintyre <george@level-n.com>
2021-12-12 17:47:10 +0100

    Conversion to aida-pva

--- src/aidasettests.m
+++ src/aidasettests.m
@@ -1,30 +1,32 @@
-aidainit
-import java.util.Vector;
-import edu.stanford.slac.aida.lib.da.DaObject; 
-d=DaObject;
-
 % TODO TRIG:LI31:109//TACT
 
 % Set KLYS TACT
-% STATUS: Untested
-name='KLYS:LI16:21//TACT'
-aidaget(name,'string',{'BEAM=1','DGRP=LIN_KLYS'})
-% SetKlysTact(name,'1','LIN_KLYS','1')
+name='KLYS:LI16:21:TACT'
+requestBuilder = pvaRequest(name)
+requestBuilder = requestBuilder.with('BEAM', 1)
+requestBuilder = requestBuilder.with('DGRP', 'LIN_KLYS')
+requestBuilder = requestBuilder.returning(AIDA_STRING)
+requestBuilder.get()
 
 % Set KLYS KPHR to exitsing value + 0.1
-% STATUS: 16-Mar-2010. Tested and working.
-name='KLYS:LI16:21//KPHR'
-kphr=aidaget(name,'double',{'BEAM=1'})
-newkphr=DaValue(java.lang.Float(kphr-0.1))
-%status = d.setDaValue(name, newkphr);
-%d.reset
+name='KLYS:LI16:21:TACT'
+requestBuilder = pvaRequest(name)
+requestBuilder = requestBuilder.returning(AIDA_DOUBLE)
+requestBuilder = requestBuilder.with('BEAM', 1)
+kphr = requestBuilder.get()
+
+requestBuilder = pvaRequest(name)
+newkphr = requestBuilder.set(kphr-0.1)
 
 % Set KLYS PDES to exitsing value + 0.1
-% STATUS: 16-Mar-2010: Tested. SLC says "Completed with ERRORS" 
-name='KLYS:LI26:31//PDES'
-pdes=aidaget(name,'double',{'BEAM=1'})
-newpdes=DaValue(java.lang.Float(pdes+0.1))
-status = d.setDaValue(name, newpdes);
-d.reset
+% STATUS: 16-Mar-2010: Tested. SLC says "Completed with ERRORS"
+name='KLYS:LI26:31:PDES'
+requestBuilder = pvaRequest(name)
+requestBuilder = requestBuilder.returning(AIDA_DOUBLE)
+requestBuilder = requestBuilder.with('BEAM', 1)
+pdes = requestBuilder.get()
+
+requestBuilder = pvaRequest(name)
+newpdes = requestBuilder.set(pdes+0.1)
 
 % BGRP
